ad2b8a523b08f1b5574caced822d372c17306526
apokalipsa44 <m_lachowicz@wp.pl>
2021-04-15 23:56:28 +0200

    node up and running

--- reader/reader.js
+++ reader/reader.js
@@ -1,8 +1,19 @@
-var fs = require('fs');
+const fs = require("fs");
+const http = require("http");
 
-fs.readFile('file.txt', 'utf8', function(err, data) {
+var express = require("express");
+var app = express();
+var path = require("path");
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+
+
+app.listen(5000, () => console.log("listening on port 5000"));
+
+fs.readFile("file.txt", "utf8", function(err, data) {
     if (err) throw err;
     console.log(data);
-
-
 });
